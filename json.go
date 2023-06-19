3f9cbae6172a3b2b263f695bc84cd76e644e4c77
brf153 <153hsb@gmail.com>
2023-06-19 11:28:38 +0530

    boilerplate

---
+++ json.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string){
+	if code > 499{
+		log.Println("Responding with 5xx error:", msg)
+	}
+	type errResponse struct{
+		Error string `json:"error"`
+	}
+	
+	respondWithJSON(w, code , errResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
+	dat, err := json.Marshal(payload)
+	if err != nil{
+		log.Println("Failed to marshal JSON response: %v", payload)
+		w.WriteHeader(code)
+		return 
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	// "github.com/brf153/rss-aggregator/vendor/github.com/go-chi/cors"
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port is not found in the environment")
+	}
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*","http://*"},
+		AllowedMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz",handlerReadiness)
+	v1Router.Get("/err",handleErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+ portString,
+	}
+
+	log.Printf("Server Starting on Port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PORT:", portString)
+}
